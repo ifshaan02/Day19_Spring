ff8aeee707d837884837a90dc7da3833328c2236
ifshaan02 <ifshaan02@gmail.com>
2025-09-22 21:44:24 +0530

    Add files via upload

---
+++ one-to-many-uni/src/main/java/com/example/cruddemojpa/dao/AppDAO.java
@@ -0,0 +1,36 @@
+package com.example.cruddemojpa.dao;
+
+import java.util.List;
+
+import com.example.cruddemojpa.entity.Course;
+import com.example.cruddemojpa.entity.Instructor;
+import com.example.cruddemojpa.entity.InstructorDetail;
+
+public interface AppDAO {
+
+	void save(Instructor theInstructor);
+	
+	Instructor findInstructorById(int theId);
+	
+	void deleteInstructorById(int theId);
+	
+	InstructorDetail findInstructorDetailById(int theId);
+	
+	void deleteInstructorDetailById(int theId);
+	
+	List<Course> findCoursesByInstructorId(int theId);
+	
+	Instructor findInstructorByIdJoinFetch(int theId);
+	
+	void update(Instructor tempInstructor);
+	
+	void update(Course tempCourse);
+	
+	Course findCourseById(int theId);
+	
+	void deleteCourseById(int theId);
+	
+	void save(Course theCourse);
+	
+	Course findCourseAndReviewsByCourseId(int theId);
+}
---
+++ one-to-many-uni/src/test/java/com/example/cruddemojpa/CruddemojpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cruddemojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemojpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
